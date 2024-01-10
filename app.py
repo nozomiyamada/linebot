4f4b23f8b23a83fd189063def0b39e7e08045d41
Nozomi Yamada <44984892+nozomiyamada@users.noreply.github.com>
2024-01-10 22:11:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def handle_message(event):
 		messages = get_official_youtube(random.choice(SONGS))
 	## MODE : LYRICS QUIZ
 	elif re.match(r'(歌詞\s*)?(クイズ|quiz)', received_message):
-		postback = 'mode=lyricsquiz&level=0&num=5&question=0&score=0&answer=NONE&asked=@correct=true'
+		postback = 'mode=lyricsquiz&level=0&num=5&question=0&score=0&answer=NONE&asked=@&correct=true'
 		messages = create_lyricsquiz_postback(postback=postback)
 	## MODE : LYRICS QUIZ
 	elif re.match(r'(intro|イントロ)(quiz|クイズ)?', received_message):
