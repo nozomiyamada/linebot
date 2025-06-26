68ceb4ccdf21b25eaf9f22e42a7a7bf66ac77d5a
Nozomi Yamada <44984892+nozomiyamada@users.noreply.github.com>
2025-06-27 01:41:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -176,4 +176,4 @@ def liffpage():
 	return render_template('index.html')
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=8000, debug=True)
+	app.run(host="0.0.0.0", port=10000, debug=True)
